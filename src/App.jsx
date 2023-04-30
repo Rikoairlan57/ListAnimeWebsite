072237231b5ca08e70735570e859c1ceead7a561
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-04-30 17:32:10 +0700

    💄 ✨ ⚡️ 🎨 created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import FirstPage from "./components/FirstPage";
+import FirstPage from "./pages/FirstPage";
 import TopAnime from "./pages/TopAnime";
 import TopManga from "./pages/TopManga";
 import SeasonalAnime from "./pages/SeasonalAnime";
 import UpcomingAnime from "./pages/UpcomingAnime";
 import AnimeSearch from "./pages/AnimeSearch";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="my-16 mx-3">
-      <Routes>
-        <Route path="/" element={<FirstPage />} />
-        <Route path="/anime/top" element={<TopAnime />} />
-        <Route path="/anime/seasonal" element={<SeasonalAnime />} />
-        <Route path="/anime/upcoming" element={<UpcomingAnime />} />
-        <Route path="/anime/search" element={<AnimeSearch />} />
-        <Route path="/manga/top" element={<TopManga />} />
-      </Routes>
+    <div className="flex flex-col">
+      <div>
+        <div className="hidden md:block h-16">
+          <Navbar />
+        </div>
+      </div>
+      <div className="my-16 mx-3">
+        <Routes>
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/anime/top" element={<TopAnime />} />
+          <Route path="/anime/seasonal" element={<SeasonalAnime />} />
+          <Route path="/anime/upcoming" element={<UpcomingAnime />} />
+          <Route path="/anime/search" element={<AnimeSearch />} />
+          <Route path="/manga/top" element={<TopManga />} />
+        </Routes>
+      </div>
     </div>
   );
 }

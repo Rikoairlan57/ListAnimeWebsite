30cea284d8cc3e1670c80810d1fa577d25a256c5
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-05-03 16:11:12 +0700

    ✅ ✨ 🎨 created search anime by title

--- src/pages/AnimeSearch.jsx
+++ src/pages/AnimeSearch.jsx
@@ -1,7 +1,44 @@
-import React from "react";
+import { useGetAnimeSearchQuery } from "../redux/services/jikanMoeApi";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import { Page, Error } from "../components";
 
 const AnimeSearch = () => {
-  return <div>AnimeSearch</div>;
+  const [searchParams] = useSearchParams({ q: " " });
+  const pageNum = 0;
+  const [page, setPage] = useState(0);
+  const query = searchParams.get("q");
+  const [type, setType] = useState("");
+  const { data, isFetching, error } = useGetAnimeSearchQuery({ type, query });
+
+  if (error) return <Error />;
+
+  return (
+    <>
+      <>
+        <h2 className="text-white text-center text-sm md:text-xl font-bold notranslate">
+          Search : {query}
+        </h2>
+        <select
+          className="bg-grey text-white p-2 text-sm mt-0 md:mt-2 notranslate"
+          onChange={(e) => setType(e.target.value)}
+        >
+          <option value="">All</option>
+          <option value="tv">TV</option>
+          <option value="movie">Movie</option>
+        </select>
+        <Page
+          data={data}
+          isFetching={isFetching}
+          error={error}
+          pageNum={pageNum}
+          setPage={(num) => setPage(num)}
+          page={page}
+        />
+      </>
+    </>
+  );
 };
 
 export default AnimeSearch;

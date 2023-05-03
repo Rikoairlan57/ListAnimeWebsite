32cd73f55b0f099edf16cecfb2e68b1f3bb14385
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-05-03 16:00:31 +0700

    💄 ✨ ⚡️ 🎨 created page upcoming anime

--- src/pages/UpcomingAnime.jsx
+++ src/pages/UpcomingAnime.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import { useGetUpcomingAnimePageQuery } from "../redux/services/jikanMoeApi";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import { Page, Error } from "../components";
 
 const UpcomingAnime = () => {
-  return <div>UpcomingAnime</div>;
+  const [searchParams, setSearchParams] = useSearchParams({ page: 1 });
+  const pageNum = searchParams.get("page");
+  const [page, setPage] = useState(parseInt(pageNum));
+  const { data, isFetching, error } = useGetUpcomingAnimePageQuery(page);
+
+  useEffect(() => {
+    setSearchParams({ page: page });
+  }, [page]);
+
+  if (error) return <Error />;
+  return (
+    <>
+      <h2 className="text-white text-center text-sm md:text-xl font-bold">
+        Upcoming Anime
+      </h2>
+      <Page
+        data={data}
+        isFetching={isFetching}
+        pageNum={pageNum}
+        setPage={(num) => setPage(num)}
+        page={page}
+      />
+    </>
+  );
 };
 
 export default UpcomingAnime;

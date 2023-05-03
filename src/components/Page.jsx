8be2e011c7bf6399259c3b1d3c576987ba0bcf31
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2023-05-03 15:48:20 +0700

    💄 ⚡️ 🎨 created seasonal page anime in season this

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -13,7 +13,7 @@ const Page = ({ data, isFetching, setPage, page }) => {
 
   if (page < 4) {
     if (totalPage < 7) {
-      for (let i = 1; i <= totalPagel; i++) {
+      for (let i = 1; i <= totalPage; i++) {
         pageArr.push(i);
       }
     } else {
@@ -37,21 +37,46 @@ const Page = ({ data, isFetching, setPage, page }) => {
     <>
       <hr className="w-full my-2 md:my-4 opacity-60 text-white" />
       <div className="flex flex-row flex-wrap justify-center">
-        <div className="flex flex-row flex-wrap justify-center">
-          {isFetching ? (
-            <PageLoad />
-          ) : (
-            dataList?.map((anime) => (
-              <div
-                key={anime.mal_id}
-                className="min-w-[20%] max-w-[20%] md:max-w-[15%] md:min-w-[15%] mx-2 mb-[10px] md:mx-6 md:mb-6]"
-              >
-                <Card data={anime} />
-              </div>
-            ))
-          )}
-        </div>
+        {isFetching ? (
+          <PageLoad />
+        ) : (
+          dataList?.map((anime) => (
+            <div
+              key={anime.mal_id}
+              className="min-w-[20%] max-w-[20%] md:max-w-[15%] md:min-w-[15%] mx-2 mb-[10px] md:mx-6 md:mb-6]"
+            >
+              <Card data={anime} />
+            </div>
+          ))
+        )}
       </div>
+      {page !== 0 && (
+        <div className="flex flex-row text-center mb-5 text-white justify-center items-center">
+          <button onClick={() => page !== 1 && setPage(page - 1)}>
+            {" "}
+            <RiArrowLeftSLine className="2xl:w-8 h-8" />
+          </button>
+          {pageArr.map((pageNum) => (
+            <button key={`page-${pageNum}`}>
+              <div
+                className={`mx-1 ${[
+                  pageNum === page
+                    ? "font-bold mx-3 text-base 2xl:text-xl opacity-100"
+                    : "font-light text-sm 2xl:text-lg opacity-60",
+                ]}`}
+                onClick={() => setPage(pageNum)}
+              >
+                {pageNum}
+              </div>
+            </button>
+          ))}
+          <button
+            onClick={() => (hasNextPage ? setPage(page + 1) : setPage(1))}
+          >
+            <RiArrowRightSLine className="2xl:w-8 h-8" />
+          </button>
+        </div>
+      )}
     </>
   );
 };
